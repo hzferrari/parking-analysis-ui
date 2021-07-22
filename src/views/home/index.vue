7b492b48d733fa8032bf72389c66a4b8d9146fc0
Zhen, Chen <Chen.Zhen@desay-svautomotive.com>
2021-07-22 11:27:57 +0800

    Chore: 调整文件位置

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,7 +7,7 @@
       @loaded="onChart1Loaded"
     ></chart-1>
 
-    <!-- <chart-2 v-if="showChart.chart2" style="margin-top: 100px"></chart-2> -->
+    <chart-2 v-if="showChart.chart2" style="margin-top: 100px"></chart-2>
 
     <!-- <chart-3 v-if="showChart.chart3" style="margin-top: 100px"></chart-3> -->
 
@@ -18,10 +18,10 @@
 </template>
 
 <script>
-import chart1 from "./chart1.vue";
-import chart2 from "./chart2.vue";
-import chart3 from "./chart3.vue";
-import chart4 from "./chart4.vue";
+import chart1 from "./chart1";
+import chart2 from "./chart2";
+import chart3 from "./chart3";
+import chart4 from "./chart4";
 
 export default {
   name: "home-page",
